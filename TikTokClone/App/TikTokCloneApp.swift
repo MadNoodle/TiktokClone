1bc860ed78e8a51f985c602cd99e85817d85316e
mathieu janneau <mjanneau@gamil.com>
2024-01-09 21:32:17 +0100

    initial foder structure

---
+++ TikTokClone/App/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by User on 9.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
