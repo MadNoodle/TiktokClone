9df961501c89e20f4d74deeb8fa360b66fa55a46
mathieu janneau <mjanneau@gamil.com>
2024-01-10 16:38:24 +0100

    feed ui setup

---
+++ TikTokClone/Core/Feed/FeedCell.swift
@@ -0,0 +1,100 @@
+//
+//  FeedCell.swift
+//  TikTokClone
+//
+//  Created by User on 10.1.2024.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    let post: Int
+    var body: some View {
+        ZStack {
+            Rectangle()
+                    .containerRelativeFrame([.horizontal, .vertical])
+                    .overlay {
+                        Text("Post \(self.post)")
+                            .foregroundStyle(.white)
+                    }
+            
+            VStack {
+                Spacer()
+                HStack(alignment: .bottom) {
+                    
+                    VStack(alignment: .leading) {
+                        Text("carlos.sainz")
+                            .fontWeight(.semibold)
+                        Text("Rocket ship prepare for take off")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.white)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 28) {
+                        Circle()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.gray)
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bookmark.fill")
+                                .resizable()
+                                .frame(width: 22, height: 28)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrowshape.turn.up.right.fill")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+                .padding(.bottom, 80)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    FeedCell(post: 0)
+}
